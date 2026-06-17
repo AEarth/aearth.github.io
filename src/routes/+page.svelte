26833e856fce9d41ca824ff1223c59fb2e324818
Aearth <aearth@users.noreply.github.com>
2026-06-16 22:17:51 -0400

    Homoe page css refinements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,14 @@
 	import GrafanaImg from '$lib/assets/images/grafana-img.png';
 	import { Badge } from '$lib/components/ui/badge';
 	import { Map, Cog, Download, Palette } from '@lucide/svelte';
-	import { abort } from 'process';
 </script>
 
 <div class="flex min-h-full flex-col gap-4 overflow-y-auto md:h-full md:overflow-hidden md:p-2">
 	<!-- flex-[2] makes this box take up 2 portions -->
-	<a href="/full-stack" class="flex-2 rounded-xl bg-muted/50">
+	<a
+		href="/full-stack"
+		class="flex-2 rounded-xl bg-muted/50 ring-1 ring-transparent transition-all duration-300 hover:bg-muted/40 hover:ring-border"
+	>
 		<!-- Content Section -->
 		<div class="relative z-10 px-2 md:px-8">
 			<!-- Title -->
@@ -19,10 +21,14 @@
 			</h1>
 			<div class="flex flex-col items-center justify-center">
 				<div class="w-full max-w-4xl">
-					<div class="overflow-hidden rounded-2xl border bg-card shadow-2xl">
+					<div class="mask-y-from-95% mask-y-to-100% mask-x-from-95% mask-x-to-100%">
 						<!-- Banner Image with Overlay -->
 						<div class="relative w-full">
-							<img src={HomeBanner} alt="Teramap Banner" class="h-36 w-full object-cover md:h-48" />
+							<img
+								src={HomeBanner}
+								alt="Teramap Banner"
+								class="h-36 w-full object-cover transition-transform duration-500 group-hover:scale-101 md:h-48"
+							/>
 							<div
 								class="absolute inset-0 bg-linear-to-t from-card via-transparent to-transparent"
 							></div>
@@ -69,7 +75,7 @@
 				</ul>
 				<img
 					src={GrafanaImg}
-					class="mx-auto mt-4 w-64 rounded-2xl border bg-card shadow-2xl"
+					class="mx-auto w-64 rounded-2xl mask-y-from-90% mask-y-to-100% mask-x-from-90% mask-x-to-100% shadow-xl"
 					alt="Grafana Screenshot"
 				/>
 			</div>
@@ -77,25 +83,43 @@
 	>
 
 	<div class="grid flex-1 grid-cols-3 gap-2 md:gap-4">
-		<a href="/geospatial" class="rounded-xl bg-muted/50"
-			><h1 class="m-1 mx-2 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
+		<a
+			href="/geospatial"
+			class="group flex flex-col overflow-hidden rounded-xl bg-muted/50 ring-1 ring-transparent transition-all duration-300 hover:bg-muted/40 hover:ring-border"
+		>
+			<h1 class="mx-3 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
 				Geospatial
 			</h1>
-			<div class="relative w-full">
-				<img src={SWVAMap} alt="Teramap Banner" class="h-36 w-full object-cover md:h-48" />
-				<div class="absolute inset-0 bg-linear-to-t from-card via-transparent to-transparent"></div>
-			</div></a
+			<div class="mt-2 w-full overflow-hidden rounded-b-xl">
+				<img
+					src={SWVAMap}
+					alt="Geospatial map"
+					class="h-32 w-full mask-y-from-80% mask-y-to-90% mask-x-from-90% mask-x-to-95% object-cover transition-transform duration-500 group-hover:scale-101 md:h-44"
+				/>
+			</div>
+		</a>
+		<a
+			href="/landscape"
+			class="group flex flex-col overflow-hidden rounded-xl bg-muted/50 ring-1 ring-transparent transition-all duration-300 hover:bg-muted/40 hover:ring-border"
 		>
-		<a href="/resume" class="rounded-xl bg-muted/50"
-			><h1 class="mx-2 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
+			<h1 class="mx-3 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
 				Landscape
 			</h1>
-			<img src={Carverponics} alt="Teramap Banner" class="h-36 w-full object-cover md:h-48" /></a
+			<div class="mt-2 w-full overflow-hidden rounded-b-xl">
+				<img
+					src={Carverponics}
+					alt="Landscape aquaponics"
+					class="h-32 w-full mask-y-from-80% mask-y-to-90% mask-x-from-90% mask-x-to-95% object-cover transition-transform duration-500 group-hover:scale-101 md:h-44"
+				/>
+			</div>
+		</a>
+		<a
+			href="/community"
+			class="group flex flex-col overflow-hidden rounded-xl bg-muted/50 ring-1 ring-transparent transition-all duration-300 hover:bg-muted/40 hover:ring-border"
 		>
-		<a href="/community" class="rounded-xl bg-muted/50">
-			<h1 class="m-1 mx-2 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
-				Resume
-			</h1></a
-		>
+			<h1 class="mx-3 flex justify-start pt-2 text-lg text-muted-foreground md:text-xl">
+				Community
+			</h1>
+		</a>
 	</div>
 </div>
